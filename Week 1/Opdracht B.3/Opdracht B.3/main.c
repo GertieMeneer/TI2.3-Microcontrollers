23875aa55a9cb59240a0395aa5602784ad9a99d3
GertieMeneer <gertjan.vat@icloud.com>
2024-02-02 12:04:52 +0100

    fixed b.3

--- Week 1/Opdracht B.3/Opdracht B.3/main.c
+++ Week 1/Opdracht B.3/Opdracht B.3/main.c
@@ -1,20 +1,35 @@
 #include <avr/io.h>
 #include <util/delay.h>
+void wait(int ms)
+{
+	for (int i = 0; i < ms; i++)
+	{
+		_delay_ms(1); // library function (max 30 ms at 8 MHz)
+	}
+}
 
-int main(void){
-    DDRD |= _BV(DDD7); // Set PORTD.7 as output
-    DDRC &= ~_BV(DDC0); // Clear PORTC.0; set as input
-    
-    while(1){
-        if ((PINC & _BV(PC0))){ // Check if button is pressed
-            while(1){
-                PORTD ^= _BV(PD7); // Toggle LED on PORTD.7
-                _delay_ms(500);   // Delay for half second
-				if (!(PINC & _BV(PC0))) break;
-            }
-        } else {
-            PORTD &= ~_BV(PD7); // Turn off LED on PORTD.7
-            _delay_ms(1);       // Minimal delay
-        }
-    }
+int main(void)
+{
+	DDRD = 0b11111111; // All pins PORTD are set to output
+	DDRC = 0b11111110;
+
+	while (1)
+	{
+		if (PINC & 0x01)
+		{
+			while (1)
+			{
+				PORTD = 0x80;
+				_delay_ms(500);
+				PORTD = 0x00;
+				_delay_ms(500);
+				if (!(PINC & 0x01)) break;
+			}
+		}
+		else
+		{
+			PORTD = 0x00;
+			_delay_ms(1);
+		}
+	}
 }
---
+++ Week 1/Opdracht B.4/Opdracht B.4/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/2/2024 11:31:21 AM
+ *  Author: Gerti
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
