3b6843d83f40e0ba45859cf2932db05d8a89afb1
GertieMeneer <gertjan.vat@icloud.com>
2024-02-02 12:48:16 +0100

    opdracht B.4

--- Week 1/Opdracht B.4/Opdracht B.4/main.c
+++ Week 1/Opdracht B.4/Opdracht B.4/main.c
@@ -1,16 +1,54 @@
-/*
- * main.c
- *
- * Created: 2/2/2024 11:31:21 AM
- *  Author: Gerti
- */ 
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** main.c
+**
+** Beschrijving:	Flash PORTD.7 when PORTC.0 is pressed
+** Target:			AVR mcu
+** Author: 			maartenwn@gmail.com en aaron.israëls@home.nl
+** -------------------------------------------------------------------------*/
+#define F_CPU 8000000
 
-#include <xc.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
-int main(void)
+
+
+
+/******************************************************************/
+void wait( int ms )
+/* 
+short:			Busy wait number of millisecs
+inputs:			int ms (Number of millisecs to busy wait)
+outputs:	
+notes:			Busy wait, not very accurate. Make sure (external)
+				clock value is set. This is used by _delay_ms inside
+				util/delay.h
+Version :    	DMK, Initial code
+*******************************************************************/
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
+/******************************************************************/
+
+int main(void) {
+	DDRD = 0xff;
+	PORTD = 0b00000001;
+	
+	while (1) {
+		wait(100);
+		PORTD = PORTD << 1;
+		if(PORTD & 0x80)
+		{
+			wait(500);
+			PORTD = 0b00000001;
+		}
+	}
+	return 0;
 }
