1059129e9b013454c50c61ed1a8c6df015d8ab74
Stef Kraaijeveld <itiredplease@gmail.com>
2024-02-03 11:30:09 +0100

    Made some changes to comments

--- Week 1/Opdracht B.4/Opdracht B.4/main.c
+++ Week 1/Opdracht B.4/Opdracht B.4/main.c
@@ -1,41 +1,15 @@
-/* ---------------------------------------------------------------------------
-** This software is in the public domain, furnished "as is", without technical
-** support, and with no warranty, express or implied, as to its usefulness for
-** any purpose.
-**
-** main.c
-**
-** Beschrijving:	Flash PORTD.7 when PORTC.0 is pressed
-** Target:			AVR mcu
-** Author: 			maartenwn@gmail.com en aaron.israëls@home.nl
-** -------------------------------------------------------------------------*/
 #define F_CPU 8000000
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-
-
-/******************************************************************/
 void wait( int ms )
-/* 
-short:			Busy wait number of millisecs
-inputs:			int ms (Number of millisecs to busy wait)
-outputs:	
-notes:			Busy wait, not very accurate. Make sure (external)
-				clock value is set. This is used by _delay_ms inside
-				util/delay.h
-Version :    	DMK, Initial code
-*******************************************************************/
 {
 	for (int i=0; i<ms; i++)
 	{
 		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
 	}
 }
-
-/******************************************************************/
 
 int main(void) {
 	DDRD = 0xff;
