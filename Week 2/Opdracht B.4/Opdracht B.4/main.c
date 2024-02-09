fdc1b0b90f3c7744e111442e3ede12b0abf2c12e
GertieMeneer <gertjan.vat@icloud.com>
2024-02-09 16:30:48 +0100

    added comments

--- Week 2/Opdracht B.4/Opdracht B.4/main.c
+++ Week 2/Opdracht B.4/Opdracht B.4/main.c
@@ -2,25 +2,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-const unsigned char numbers [10] = {
-	0b00000001, // 0
-	0b00000011, // 1
-	0b00000111, // 2
-	0b00001110, // 3
-	0b00011100, // 4
-	0b00111000, // 5
-	0b00110001, // 6
-	0b00100011, // 7
-	0b00000011, // 8
+const unsigned char numbers [10] = {		//all "frames" of the snake crawling
+	0b00000001,
+	0b00000011,
+	0b00000111,
+	0b00001110,
+	0b00011100,
+	0b00111000,
+	0b00110001,
+	0b00100011,
+	0b00000011,
 };
 
 int main( void ) {
-	DDRD = 0b11111111; // PORTD all output
+	DDRD = 0b11111111; //PORTD all output
 	int index = 0;
 	
+	//iterate through array
 	while (1) {
 		
-		// Write data to PORTD
+		//write "frame" to 7 segment
 		PORTD = numbers[index];
 		
 		_delay_ms(150);
