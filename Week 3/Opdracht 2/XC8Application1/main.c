c41f1651836338631655fecdd1a09c9c5031e5c7
GertieMeneer <gertjan.vat@icloud.com>
2024-03-08 10:53:52 +0100

    fix week 3 b.2
    Code works better now

--- Week 3/Opdracht 2/XC8Application1/main.c
+++ Week 3/Opdracht 2/XC8Application1/main.c
@@ -1,53 +1,55 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
 
-volatile uint8_t pulse_high = 1; // Indicator voor hoog- en laagperiode van de puls
+#define PRESCALER_VALUE 1024
 
-// initialiseer timer/counter 2 in CTC-modus
-void timer2_init() {
-	// zet timer/counter in ctc-modus
-	TCCR2 |= (1 << WGM21);
-	
-	// prescaler op 1024
-	TCCR2 |= (1 << CS22) | (1 << CS21);
-	
-	OCR2 = 146; // prescaler 1024: 15 ms = 146 pulsen
-	
-	// interrupt voor compare match A
-	TIMSK |= (1 << OCIE2);
-}
+#define HIGH_DURATION 389
+#define LOW_DURATION 233
 
-// isr voor timer/counter compare match A
-ISR(TIMER2_COMPA_vect) {
-	if (pulse_high) {
-		// portd 7 hoog
-		PORTD |= (1 << PD7);
-		
-		OCR2 = 244; // prescaler 1024: 25 ms = 244 pulsen
-		pulse_high = 0;
-		} else {
-		// portd 7 laag
-		PORTD &= ~(1 << PD7);
-		
-		// terug naar hoogpuls na 25 ms
-		OCR2 = 146; // prescaler 1024: 15 ms = 146 pulsen
-		pulse_high = 1;
-	}
-}
+
+void setup_timer();
+void setup_io();
 
 int main() {
-	// portd 7 als uitgang
-	DDRD |= (1 << PD7);
-
-	timer2_init();
-
-	// global interupts aanzetten
+	setup_timer(); //init timer
+	setup_io();    //init i/0
+	
+	//enable global interrupts
 	sei();
 
 	while (1) {
-		_delay_ms(1);
 	}
 
 	return 0;
+}
+
+void setup_timer() {
+	//set timer/counter 2 to ctc mode
+	TCCR2 = (1 << WGM21);
+	
+	TCCR2 |= (1 << CS22) | (1 << CS20);
+
+	OCR2 = HIGH_DURATION;
+
+	//enable timer/counter 2 ocm interrupt
+	TIMSK |= (1 << OCIE2);
+}
+
+void setup_io() {
+	//portd 7 output
+	DDRD |= (1 << PD7);
+
+	//portd 7 low so tart
+	PORTD &= ~(1 << PD7);
+}
+
+//timer/counter 2 ocm isr
+ISR(TIMER2_COMP_vect) {
+	PORTD ^= (1 << PD7);
+
+	if (PORTD & (1 << PD7)) {
+		OCR2 = HIGH_DURATION;
+		} else {
+		OCR2 = LOW_DURATION;
+	}
 }
