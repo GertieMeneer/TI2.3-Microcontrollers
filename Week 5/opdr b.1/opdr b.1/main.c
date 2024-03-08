996fc5342b8812a15d9f6797110cff35c7e40c24
GertieMeneer <gertjan.vat@icloud.com>
2024-03-08 13:52:37 +0100

    fix week 5 opdr 5.1
    works

--- Week 5/opdr b.1/opdr b.1/main.c
+++ Week 5/opdr b.1/opdr b.1/main.c
@@ -3,8 +3,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-
 #define BIT(x) (1 << (x))
 #define DDR_SPI DDRB
 #define PORT_SPI PORTB
@@ -19,6 +17,7 @@ void wait(int ms) {
 	}
 }
 
+//init spi as master
 void spi_masterInit(void) {	
 	DDR_SPI = 0xff;
 	DDR_SPI &= ~BIT(SPI_MISO);
@@ -26,19 +25,23 @@ void spi_masterInit(void) {
 	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1);
 }
 
+//write to spi
 void spi_write(unsigned char data) {
 	SPDR = data;
 	while (!(SPSR & BIT(SPIF)));
 }
 
+//select slave
 void spi_slaveSelect(unsigned char chipNumber) {
 	PORTB &= ~BIT(chipNumber);
 }
 
+//unselect slave
 void spi_slaveDeSelect(unsigned char chipNumber) {
 	PORTB |= BIT(chipNumber);
 }
 
+//init display
 void displayDriverInit() {
 	spi_slaveSelect(0);
 	spi_write(0x09);
@@ -47,20 +50,26 @@ void displayDriverInit() {
 
 	spi_slaveSelect(0);
 	spi_write(0x0A);
-	spi_write(0x0F); // Max intensity
+	spi_write(0x0F); //brightness, max intensity
 	spi_slaveDeSelect(0);
 
 	spi_slaveSelect(0);
 	spi_write(0x0B);
-	spi_write(0x03); // Display digits 0..3
+	spi_write(0x03); //all display digits
 	spi_slaveDeSelect(0);
 
 	spi_slaveSelect(0);
 	spi_write(0x0C);
 	spi_write(0x01);
 	spi_slaveDeSelect(0);
+	
+	//^^^ these 4 blocks can be replaced with spi_writeWord,
+	//but to show the brightness and selecting all digits
+	//we leave them like this.
 }
 
+
+//default commands for writing data
 void spi_writeWord(unsigned char address, unsigned char data) {
 	spi_slaveSelect(0);
 	spi_write(address);            // Write address byte
@@ -68,19 +77,18 @@ void spi_writeWord(unsigned char address, unsigned char data) {
 	spi_slaveDeSelect(0);          // Deselect display chip
 }
 
-
 int main() {
 	DDRB = 0x01;
 	spi_masterInit();
 	displayDriverInit();
 
-	// Clear display
+	//clear display (set 0's)
 	for (char i = 1; i <= 4; i++) {
 		spi_writeWord(i, 0);
 	}
 	wait(1000);
 
-	// Write 1-2-3-4 to all four digits
+	//showing 1-2-3-4 on all digits
 	for (char i = 1; i <= 4; i++) {
 		spi_writeWord(i, i);
 		wait(1000);
