bc1ae4d5e3d183f515db131377d0c90c49ab2ce8
GertieMeneer <gertjan.vat@icloud.com>
2024-03-08 09:32:03 +0100

    Fix week 2 opdr b.2
    Implemented feedback so it's a looplicht now

--- Week 2/Opdracht B.2/Opdracht B.2/main.c
+++ Week 2/Opdracht B.2/Opdracht B.2/main.c
@@ -1,20 +1,41 @@
-#define F_CPU 10000000
+#define F_CPU 8000000
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+#define NUM_LEDS 8
+
+volatile uint8_t activeLedIndex = 0;
+
 ISR(INT0_vect) {		//interrupt code 0
-	//set led rd 4 on
-	PORTD = 0x10;
+	if (activeLedIndex == (NUM_LEDS - 1)) {
+		//reached the end, move to beginning
+		activeLedIndex = 0;
+		} else {
+		//move to next led
+		activeLedIndex++;
+	}
+
+	//turn on led
+	PORTC = (1 << activeLedIndex << 1) & 0xFE;
 }
 
 ISR(INT1_vect) {		//interrupt code 1
-	//set led rd 4 off
-	PORTD = 0x00;
+	if (activeLedIndex == 0) {
+		//reached beginning, move to end
+		activeLedIndex = NUM_LEDS - 1;
+		} else {
+		//move to previous led
+		activeLedIndex--;
+	}
+
+	//turn on led
+	PORTC = (1 << (activeLedIndex + 1)) & 0xFE;
 }
 
 int main(void) {
-	DDRD = 0xF0; //set ins and outs
+	DDRD = 0xF0;
+	DDRC = 0xFF;
 
 	EICRA |= (1 << ISC10) | (1 << ISC11) | (1 << ISC00) | (1 << ISC01); //set bits in external interrupt control register to run trigger code
 	EIMSK |= (1 << INT1) | (1 << INT0); //set bits in external interrupt mask register to turn on interrupt requests
@@ -22,7 +43,7 @@ int main(void) {
 	sei(); //enable global interrupt system
 
 	while (1) {
-		//empty :)
+		// empty :)
 	}
 
 	return 1;
