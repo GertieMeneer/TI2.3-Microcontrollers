5b59f7140d89b517801a1b9f9931dddeb6f18e2d
GertieMeneer <gertjan.vat@icloud.com>
2024-03-15 09:13:42 +0100

    Eindopdracht
    Made eindopdracht dir and file

---
+++ Eindopdracht/Eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ * Created: 3/15/2024 9:12:40 AM
+ *  Author: GertieMeneer
+ */ 
+
+#define F_CPU 8e6
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
