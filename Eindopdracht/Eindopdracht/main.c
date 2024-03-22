a96b41d3fcfff4c35b58a07408ce394b9fb1042e
GertieMeneer <gertjan.vat@icloud.com>
2024-03-22 13:20:17 +0100

    High/Low values now show on LCD
    Finally

--- Eindopdracht/Eindopdracht/main.c
+++ Eindopdracht/Eindopdracht/main.c
@@ -29,19 +29,39 @@ int main(void) {
 	clearDisplay();
 	_delay_ms(1000);
 	
+	int highest = 0;
+	int lowest = 100;
+	
+	int temperature = (ADCH * 5000UL) / 256;
+	highest = temperature;
+	lowest = temperature;
+	
 	while(1) {
 		int temperature = (ADCH * 5000UL) / 256;
-		char buffer[20];
 		
-		sprintf(buffer, "Temp: %d", temperature);
+		if(temperature > highest) {
+			highest = temperature;
+		}
+		if(temperature < lowest) {
+			lowest = temperature;
+		}
+		char highBuffer[20];
+		char lowBuffer[20];
 		
-		lcd_write_string(buffer);
-
+		sprintf(highBuffer, "High: %d", highest);
+		lcd_write_string(highBuffer);
+		_delay_ms(10);
+		lcd_write_command(192);
+		_delay_ms(10);
+		sprintf(lowBuffer, "Low: %d", lowest);
+		lcd_write_string(lowBuffer);
+		_delay_ms(10);
+		
 		writeLedDisplay(temperature);
 
 		_delay_ms(1000);
 		lcd_write_command(0x01);
-		_delay_ms(1000);
+		_delay_ms(5);
 		
 	}
 
